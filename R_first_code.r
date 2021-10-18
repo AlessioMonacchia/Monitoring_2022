857b7657380f5e0c74a9acc44705ec85742d6a5c
AlessioMonacchia <92572677+AlessioMonacchia@users.noreply.github.com>
2021-10-18 10:57:37 +0200

    Update R_first_code.r

--- R_first_code.r
+++ R_first_code.r
@@ -7,3 +7,10 @@ water<-c(100,200,300,400,500)
 fishes<-c(10,50,60,100,200)
 #plot diversity of fishes on the y against the water on the x
 plot(water, fishes)
+
+#third lecture
+#build a data frame/table in R with data.frame function, also assigning it a name
+streams <- data.frame(water, fishes)
+streams
+#view function will show thw table in a different manner
+View(streams)
